3983bfee6b38ff3f4686bc5170c788fa3789af6a
hidecard <hidecard1500@gmail.com>
2025-07-02 22:39:43 +0630

    img

--- usercreate.php
+++ usercreate.php
@@ -2,13 +2,20 @@
 
 include "db.php";
 
-
 if(isset($_POST['save'])){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $sql = "INSERT INTO users(name,email,password) VALUES('$name','$email','$password')";
+    //image upload
+    $img_name = $_FILES['img']['name']; // from input img=>input name , name =>img name
+    $tmp_name = $_FILES['img']['tmp_name']; // အပေါ်က img data ကို ယာယီသိမ်းပေးတာ
+
+    $folder = "images/".$img_name; // image သိမ်းဖို့ Folder ဆောက်တာ
+   move_uploaded_file($tmp_name,$folder); // သိမ်းဆည်းတာ
+
+
+    $sql = "INSERT INTO users(name,email,password,image) VALUES('$name','$email','$password','$folder')";
     $res = mysqli_query($conn,$sql);
 
     if($res){
@@ -26,10 +33,11 @@ if(isset($_POST['save'])){
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="post">
+    <form action="" method="post" enctype="multipart/form-data">
         <input type="text" name="name" id="">
         <input type="email" name="email" id="">
         <input type="password" name="password" id="">
+        <input type="file" name="img" id="">
         <button name="save">Add</button>
     </form>
 </body>
